463621fe23b625c70160243544229951227e1d93
Tadej Murovec <tadej.murovec@gmail.com>
2020-07-28 08:41:51 +0200

    Specify default mailer host

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module GhostBlazer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
+    config.action_mailer.default_url_options = { host: ENV.fetch("APP_HOST") }
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
